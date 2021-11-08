b4ee432cac7c2128eae9b2d0876c546385b7940e
Shubham <shubham30898@gmail.com>
2021-11-08 15:14:08 +0530

    Assignment

--- Challenge/Assignment2.java
+++ Challenge/Assignment2.java
@@ -1,3 +1,15 @@
 public class Assignment2 {
+    public static String convert(String str, int numberOfRows) {
+        if ((numberOfRows == 1) || (str.length() <= numberOfRows))
+            return str;
 
+    }
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        String str = input.nextLine();
+        int numberOfRows = input.nextInt();
+        String str2 = convert(str, numberOfRows);
+        System.out.println(str2);
+    }
 }
