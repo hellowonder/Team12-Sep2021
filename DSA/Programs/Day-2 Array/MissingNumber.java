2eae22a441dcf2b8bfc3cb694a58065ce51af563
Shubham <shubham30898@gmail.com>
2021-11-08 15:49:40 +0530

    update

--- DSA/Programs/Day-2 Array/MissingNumber.java
+++ DSA/Programs/Day-2 Array/MissingNumber.java
@@ -4,15 +4,24 @@ import java.util.Scanner;
 
 public class MissingNumber {
     public static void main(String[] args) {
-	int arr[] = {1 , 2, 3, 5, 6};
-	int n = arr.length + 1;
+        // int arr[] = {1 , 2, 3, 5, 6};
+        int size;
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter the size of the array");
+        size = input.nextInt();
+        int arr[] = new int[size];
+        System.out.println("Enter Elements in array: ");
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = input.nextInt();
+        }
+        int n = arr.length + 1;
         int miss_no = 0;
 
         int totalSum = n * (n + 1) / 2;
         int sumOfArray = 0;
 
         for (int i = 0; i < arr.length; i++) {
-            sumOfArray = sumOfArray+ arr[i];
+            sumOfArray = sumOfArray + arr[i];
         }
         System.out.println("Missing Element is: " + (totalSum - sumOfArray));
     }
